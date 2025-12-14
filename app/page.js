604200ced637183b28100885f95d9e56b66edb15
puturangga21 <puturangga21@gmail.com>
2025-12-14 12:49:52 +0800

    feat: Implement core UI components including Button, Card, Input, and establish global styling and utilities.

--- app/page.js
+++ app/page.js
@@ -1,19 +1,10 @@
-import prisma from '@/lib/prisma';
+import { Button } from '@/components/ui/button';
 
-export default async function Home() {
-  const customer = await prisma.customer.findMany();
-
+export default function Page() {
   return (
-    <section className="max-w-4xl mx-auto p-4">
-      {customer.length > 0 ? (
-        <ul className="list-disc">
-          {customer.map((customer) => (
-            <li key={customer.customer_id}>{customer.fullname}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No customers found</p>
-      )}
-    </section>
+    <main>
+      <h1>hello</h1>
+      <Button>Button</Button>
+    </main>
   );
 }
